6c31657d195a5f834d72ddba14bae13c67851b7a
Jordi <jordibeco1995@gmail.com>
2017-10-20 19:57:05 +0200

    pujant l'arxiu.java

--- src/NumerosAleatoris.java
+++ src/NumerosAleatoris.java
@@ -1,7 +1,15 @@
-package PACKAGE_NAME;
-
-/**
- * Created by Jordi on 20/10/2017.
- */
+import java.util.*;
 public class NumerosAleatoris {
+    public static void main(String[] args) {
+        Random aleatori = new Random();
+        int numero = 14321;
+
+        List<Integer> llista_nums = new ArrayList<>();
+
+        for (int i = 0; i < 500000 ; i++) {
+            int rand = aleatori.nextInt(1000000);
+        }
+
+        System.out.println(llista_nums.contains(numero));
+    }
 }
